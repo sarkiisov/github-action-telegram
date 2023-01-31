b2369c596fd50336a5d7ee15ec604a3681d3acf8
sarkisovTu <sarkisov1337@gmail.com>
2023-01-31 17:12:10 +0300

    fix: workflow config

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const formattedSecret = `Secret variable : ${secret}`;
 (async function() {
   const bot = new Bot('5942682565:AAGYzxL0zSMr7_AI0w3nT02tk_PBexfZSn8');
 
-  bot.sendMessage(-619418505, formattedSecret);
+  bot.sendMessage(345021341, formattedSecret);
   console.log(`Secret variable: `, formattedSecret);
 })()
