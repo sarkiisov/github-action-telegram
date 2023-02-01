2db3a8f95916b455175dde428e7907ae7668e3ab
sarkisovTu <sarkisov1337@gmail.com>
2023-02-01 15:43:55 +0300

    fix: change parse_mode to HTML

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/naming-convention */
 import * as core from '@actions/core';
 import * as github from '@actions/github';
@@ -23,13 +22,13 @@ const chatId = 345021341;
   const { action } = github.context.payload;
 
   const {
-    title, number, commits, html_url: pullUrl, additions, deletions, changed_files
+    title, number, commits, html_url: pullUrl, additions, deletions, changed_files, requested_reviewers
   } = github.context.payload.pull_request;
 
-  // const reviewers = requested_reviewers.reduce((accumulator, currentValue) => {
-  //   const line = `${currentValue.login} ${currentValue.html_url} \n`;
-  //   return accumulator + line;
-  // }, '');
+  const reviewers = requested_reviewers.reduce((accumulator, currentValue) => {
+    const line = `${currentValue.login} ${currentValue.html_url} \n`;
+    return accumulator + line;
+  }, '');
 
   const { login, html_url: senderUrl } = github.context.payload.sender;
 
@@ -52,21 +51,23 @@ const chatId = 345021341;
   // }, { additions: 0, deletions: 0 });
 
   const message = `
-Pull request ${action} by
+Pull request ${action} by <a href="${senderUrl}">${login}</a>
 
-*${title} (${number})*
+<b>${title} (${number})<b>
 
 Commits: ${commits}
 Addition: ${additions}
 Deletions: ${deletions}
 Changed files: ${changed_files}
 Reviewers:
+${reviewers}
 
+<a href="${pullUrl}">View details</a>
 `;
 
   // bot.sendMessage(chatId, `PullUrl: ${pullUrl}, Title ${title}, Body: ${body},
   // Number: ${number}, Commits: ${commits}, SenderLogin: ${login}, SenderUrl: ${senderUrl}, Additions: ${additions}, Deletions: ${deletions}, CahngedFiles: ${changed_files}, Reviewers ${reviewers}`);
-  bot.sendMessage(chatId, message, { parse_mode: 'MarkdownV2' });
+  bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
   console.log('Repo context: ', JSON.stringify(github.context.repo));
   console.log('Payload: ', JSON.stringify(github.context.payload));
   // bot.sendMessage(chatId, JSON.stringify(github.context.payload));
