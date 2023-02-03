6c548d930d066f342830ece56a2b2bd4cedffbdc
sarkisovTu <sarkisov1337@gmail.com>
2023-02-03 23:05:41 +0300

    refactor: update utils in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import Bot from 'node-telegram-bot-api';
+import { formatKeyValueString, formatReviewersArray, formatUrl } from './utils';
 
 const telegramBotToken = core.getInput('telegramBotToken');
 const telegramChatId = core.getInput('telegramChatId');
@@ -29,26 +30,19 @@ const telegramChatId = core.getInput('telegramChatId');
   const baseBranch = github.context.payload.pull_request.base.ref;
   const compareBranch = github.context.payload.pull_request.head.ref;
 
-  const formatKeyValueString = (key: string, value: string) => `<b>${key}: </b>${value}`;
-
-  const formattedReviewers = requestedReviewers.map((user) => `<a href="${user.html_url}">${user.login}</a>`).join(',\n');
-
   const notificationMessage = `
 <a href="${senderUrl}">${senderLogin}</a> created a pull request
 <a href="${pullUrl}"><b>#${pullNumber} ${pullTitle}</b></a>
 
-${formatKeyValueString('Repository', `<a href="${repositoryUrl}">${repositoryName}</a>`)}
-${formatKeyValueString('Created', createdAt.split(' ')[0])}
+${formatKeyValueString('Repository', formatUrl(repositoryName, repositoryUrl))}
+${formatKeyValueString('Created', createdAt)}
 ${formatKeyValueString('Branch', `${baseBranch} ← ${compareBranch}`)}
-${formatKeyValueString('Reviewers', formattedReviewers)}
+${formatKeyValueString('Reviewers', formatReviewersArray(requestedReviewers))}
 `;
 
   bot.sendMessage(
     telegramChatId,
     notificationMessage,
-    {
-      parse_mode: 'HTML',
-      disable_web_page_preview: true
-    }
+    { parse_mode: 'HTML', disable_web_page_preview: true }
   );
 }());
